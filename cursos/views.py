c525576cfc9a614980fec3eb6ea5759a07d61884
Ricardo Pellegrini <ricardo.pellegrini92@gmail.com>
2020-10-16 18:51:27 -0300

    adicionando viewsets e router

--- cursos/views.py
+++ cursos/views.py
@@ -1,38 +1,70 @@
-from rest_framework.views import APIView
+from rest_framework import generics   # para v1
+from rest_framework.generics import get_object_or_404   # para v1
+
+from rest_framework import viewsets   # para v2
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework import status
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
 
-class CursoAPIView(APIView):
-  """
-  API de Cursos
-  """
-  def get(self, request):
-    cursos = Curso.objects.all()
-    serializer = CursoSerializer(cursos, many=True)
+"""
+API V1
+"""
+
+# Lista, cadastra cursos
+class CursosAPIView(generics.ListCreateAPIView):
+  queryset = Curso.objects.all()
+  serializer_class = CursoSerializer
+
+
+# Coleta, atualiza e deleta um curso
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Curso.objects.all()
+  serializer_class = CursoSerializer
+
+
+# Lista e cadastra avaliações
+class AvaliacoesAPIView(generics.ListCreateAPIView):
+  queryset = Avaliacao.objects.all()
+  serializer_class = AvaliacaoSerializer
+
+  def get_queryset(self):
+    if self.kwargs.get('curso_pk'):
+      return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
+    return self.queryset.all()
+
+
+# Coleta, atualiza e deleta uma avaliação
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Avaliacao.objects.all()
+  serializer_class = AvaliacaoSerializer
+
+  def get_object(self):
+    if self.kwargs.get('curso_pk'):
+      return get_object_or_404(self.get_queryset(),
+                              curso_id=self.kwargs.get('curso_id'),
+                              pk=self.kwargs.get('avaliacao_pk')
+      )
+      return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
+
+
+"""
+API V2
+"""
+
+class CursoViewSet(viewsets.ModelViewSet):
+  queryset = Curso.objects.all()
+  serializer_class = CursoSerializer
+
+  @action(detail=True, methods=['get'])
+  def avaliacoes(self, request, pk=None):
+    curso = self.get_object()
+    serializer = AvaliacaoSerializer(curso.avaliacoes.all(), many=True)
     return Response(serializer.data)
 
-  def post(self, request):
-    serializer = CursoSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-class AvaliacaoAPIView(APIView):
-  """
-  API de Avaliações
-  """
-  def get(self, request):
-    avaliacoes = Avaliacao.objects.all()
-    serializer = AvaliacaoSerializer(avaliacoes, many=True)
-    return Response(serializer.data)
-
-  def post(self, request):
-    serializer = AvaliacaoSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
+class AvaliacaoViewSet(viewsets.ModelViewSet):
+  queryset = Avaliacao.objects.all()
+  serializer_class = AvaliacaoSerializer
